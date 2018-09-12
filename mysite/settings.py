5d79c40bf5b4acb2c2def96065af1288a20ef8b5
pilot <be2man@hotmail.com>
2018-09-12 12:01:24 -0700

    updated setting.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -97,19 +97,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'firstapp',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-# DATABASES = {}
-# DATABASES['default'] =  dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-# DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'firstapp',
+#         'USER': 'postgres',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
