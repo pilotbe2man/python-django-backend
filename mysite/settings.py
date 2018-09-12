4da155831104d9bba11d6db743488134f47d0d21
pilot <be2man@hotmail.com>
2018-09-12 12:15:28 -0700

    added PROJECT_DIR in setting.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,8 @@ import os
 import psycopg2
 import dj_database_url
 
+PROJECT_DIR  = os.path.dirname(__file__)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
